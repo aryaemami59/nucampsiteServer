2e036ea4d6ded9ba4df2a48a9ff59e06933fb45c
Arya Emami <aryaemami59@yahoo.com>
2022-10-21 19:18:28 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+const clientSecret = require("./secret");
+
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "672747664172332",
+    clientSecret,
+  },
 };
